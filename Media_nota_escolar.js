9dd4e412390e9b8462a43889a3b53a16d4b36841
Victor Alexandre Vieira de Matos <112444309+VictorAVMatos@users.noreply.github.com>
2023-04-18 21:59:00 -0300

    Add files via upload

---
+++ Media_nota_escolar.js
@@ -0,0 +1,13 @@
+const nota1 = 7;
+const nota2 = 5;
+const nota3 = 6.5;
+const media = (nota1 + nota2 + nota3) / 3;
+
+if (media < 5) {
+console.log ('Está de Reprovado')
+} else if (media >= 5 && media < 7){
+console.log ('Está de Recuperação')
+} else (media > 7) 
+{
+console.log ('Está aprovado')
+}    
